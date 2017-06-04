58ebad34ee545ae3d724230ab231e2866b64532c
Wan Shan Loo <wanshan.loo@finantix.com>
2017-06-04 19:26:55 +0800

    solution for assignment 1

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 (function () {
   'use strict';
 
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
+  angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = [$scope];
-  
-})
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    var maxItems = 3;
+    $scope.lunchInput = '';
+    $scope.sayMessage = '';
+    $scope.checkList = function () {
+      console.log($scope.lunchInput.length);
+      if ($scope.lunchInput.length === 0) {
+        $scope.sayMessage = 'Pls enter data first.'
+      } else if ($scope.lunchInput.split(',').length > maxItems) {
+        $scope.sayMessage = 'Too much!'
+      } else if ($scope.lunchInput.split(',').length <= maxItems){
+        $scope.sayMessage = 'Enjoy!'
+      }
+    }
+  }
+})();
